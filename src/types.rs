f26efa0977e7826b49136b96fe9d4acf6500b7a9
Andrew Chin <achin@eminence32.net>
2017-11-27 20:11:12 -0500

    Bring simple enums into types.rs and stop using enums from protobufs
    This lets us make our enums more rustic, and add documentation and
    implementation to them (which would have not been possible otherwise)

--- src/types.rs
+++ src/types.rs
@@ -79,18 +79,46 @@ pub struct LocalMap {
     pub MapPath: String,
 }
 
+#[derive(Clone,PartialEq,Eq,Debug,Hash,ToProtobuf,FromProtobuf)]
+pub enum PlayerType {
+    Participant = 1,
+    Computer = 2,
+    Observer = 3,
+}
+
+#[derive(Clone,PartialEq,Eq,Debug,Hash,ToProtobuf,FromProtobuf)]
+pub enum Race {
+    NoRace = 0,
+    Terran = 1,
+    Zerg = 2,
+    Protoss = 3,
+    Random = 4,
+}
+
+#[derive(Clone,PartialEq,Eq,Debug,Hash,ToProtobuf,FromProtobuf)]
+pub enum Difficulty {
+    VeryEasy = 1,
+    Easy = 2,
+    Medium = 3,
+    MediumHard = 4,
+    Hard = 5,
+    Harder = 6,
+    VeryHard = 7,
+    CheatVision = 8,
+    CheatMoney = 9,
+    CheatInsane = 10,
+}
 
 #[derive(Debug, ToProtobuf, FromProtobuf)]
 pub struct PlayerSetup {
     #[Get]
-    pub field_type: protos::PlayerType,
+    pub field_type: PlayerType,
     #[Get]
-    pub race: protos::Race,
+    pub race: Race,
     #[Get]
-    pub difficulty: protos::Difficulty,
+    pub difficulty: Difficulty,
 }
 
-pub use self::protos::{PlayerType, Race, Difficulty};
 
 #[derive(Debug, ToProtobuf, FromProtobuf)]
 #[AttachedTo(RequestCreateGame)]
@@ -121,7 +149,7 @@ pub struct RequestCreateGame {
 #[AttachedTo(RequestJoinGame)]
 pub enum Participation {
     #[Get]
-    Race(protos::Race),
+    Race(Race),
     #[Get]
     ObservedPlayerId(u32),
 }
@@ -235,14 +263,14 @@ pub struct PlayerInfo {
     #[Get]
     pub player_id: u32,
     #[Get]
-    pub field_type: protos::PlayerType,
+    pub field_type: PlayerType,
     #[Get]
-    pub race_requested: protos::Race,
+    pub race_requested: Race,
     /// Only populated for your player or when watching replay
     #[Get]
-    pub race_actual: Option<protos::Race>,
+    pub race_actual: Option<Race>,
     #[Get]
-    pub difficulty: protos::Difficulty,
+    pub difficulty: Difficulty,
 }
 
 #[derive(Debug, ToProtobuf)]
@@ -289,6 +317,15 @@ pub struct ResponseData {
     pub effects: Vec<EffectData>,
 }
 
+#[derive(Clone,PartialEq,Eq,Debug,Hash,FromProtobuf)]
+pub enum AbilityData_Target {
+    None = 1,
+    Point = 2,
+    Unit = 3,
+    PointOrUnit = 4,
+    PointOrNone = 5,
+}
+
 #[derive(Debug, FromProtobuf)]
 pub struct AbilityData {
     /// Stable ID
@@ -316,7 +353,7 @@ pub struct AbilityData {
     pub remaps_to_ability_id: u32,
 
     #[Get]
-    pub target: protos::AbilityData_Target,
+    pub target: AbilityData_Target,
 
     /// If true, the ability may be used on this set of mods/map
     #[Get]
@@ -338,8 +375,10 @@ pub struct AbilityData {
 
 #[derive(Debug, FromProtobuf)]
 pub struct UnitTypeData {
+    /// Stable ID
     #[Get]
     pub unit_id: u32,
+    /// Catalog name of the unit
     pub name: String,
     #[Get]
     pub available: bool,
@@ -357,7 +396,7 @@ pub struct UnitTypeData {
     #[Get]
     pub ability_id: u32,
     #[Get]
-    pub race: protos::Race,
+    pub race: Race,
     #[Get]
     pub build_time: f32,
     #[Get]
@@ -374,7 +413,7 @@ pub struct UnitTypeData {
     #[Get]
     pub require_attached: bool,
 
-    pub attributes: Vec<protos::Attribute>,
+    pub attributes: Vec<Attribute>,
     #[Get]
     pub movement_speed: f32,
     #[Get]
@@ -382,10 +421,18 @@ pub struct UnitTypeData {
     pub weapons: Vec<Weapon>,
 }
 
+#[derive(Clone,PartialEq,Eq,Debug,Hash,FromProtobuf)]
+#[allow(non_camel_case_types)]
+pub enum Weapon_TargetType {
+    Ground = 1,
+    Air = 2,
+    Any = 3,
+}
+
 #[derive(Debug, FromProtobuf)]
 pub struct Weapon {
     #[Get]
-    pub field_type: protos::Weapon_TargetType,
+    pub field_type: Weapon_TargetType,
     #[Get]
     pub damage: f32,
     pub damage_bonus: Vec<DamageBonus>,
@@ -400,10 +447,25 @@ pub struct Weapon {
     pub speed: f32,
 }
 
+#[derive(Clone,PartialEq,Eq,Debug,Hash, FromProtobuf)]
+pub enum Attribute {
+    Light = 1,
+    Armored = 2,
+    Biological = 3,
+    Mechanical = 4,
+    Robotic = 5,
+    Psionic = 6,
+    Massive = 7,
+    Structure = 8,
+    Hover = 9,
+    Heroic = 10,
+    Summoned = 11,
+}
+
 #[derive(Debug, FromProtobuf)]
 pub struct DamageBonus {
     #[Get]
-    pub attribute: protos::Attribute,
+    pub attribute: Attribute,
     #[Get]
     pub bonus: f32,
 }
@@ -461,12 +523,44 @@ pub enum Request {
 }
 
 
+#[derive(Clone,PartialEq,Eq,Debug,Hash,ToProtobuf,FromProtobuf)]
+pub enum ResponseCreateGame_Error {
+    MissingMap = 1,
+    InvalidMapPath = 2,
+    InvalidMapData = 3,
+    InvalidMapName = 4,
+    InvalidMapHandle = 5,
+    MissingPlayerSetup = 6,
+    InvalidPlayerSetup = 7,
+    MultiplayerUnsupported = 8,
+}
+
 #[derive(Debug, ToProtobuf, FromProtobuf)]
 pub struct ResponseCreateGame {
     #[Get]
-    pub error: Option<protos::ResponseCreateGame_Error>,
+    pub error: Option<ResponseCreateGame_Error>,
     pub error_details: Option<String>,
 }
+
+
+#[derive(Clone,PartialEq,Eq,Debug,Hash,ToProtobuf,FromProtobuf)]
+#[allow(non_camel_case_types)]
+pub enum ResponseJoinGame_Error {
+    MissingParticipation = 1,
+    InvalidObservedPlayerId = 2,
+    MissingOptions = 3,
+    MissingPorts = 4,
+    GameFull = 5,
+    LaunchError = 6,
+    FeatureUnsupported = 7,
+    NoSpaceForUser = 8,
+    MapDoesNotExist = 9,
+    CannotOpenMap = 10,
+    ChecksumError = 11,
+    NetworkError = 12,
+    OtherError = 13,
+}
+
 
 #[derive(Debug, ToProtobuf, FromProtobuf)]
 pub struct ResponseJoinGame {
@@ -474,7 +568,7 @@ pub struct ResponseJoinGame {
     pub player_id: u32,
 
     #[Get]
-    pub error: Option<protos::ResponseJoinGame_Error>,
+    pub error: Option<ResponseJoinGame_Error>,
     pub error_details: Option<String>,
 }
 
@@ -499,11 +593,16 @@ pub struct Action {
     pub chat: Vec<ActionChat>,
 }
 
+#[derive(Clone,PartialEq,Eq,Debug,Hash,FromProtobuf)]
+pub enum ActionChat_Channel {
+    Broadcast = 1,
+    Team = 2,
+}
 
 #[derive(Debug, FromProtobuf)]
 pub struct ActionChat {
     #[Get]
-    pub channel: protos::ActionChat_Channel,
+    pub channel: ActionChat_Channel,
     pub message: String,
 }
 
@@ -545,6 +644,233 @@ pub struct ActionRawCameraMove {
     pub center_world_space: Point,
 }
 
+#[derive(Clone,PartialEq,Eq,Debug,Hash,FromProtobuf)]
+#[ProtoType(Result)]
+pub enum ProtoResult {
+    Victory = 1,
+    Defeat = 2,
+    Tie = 3,
+    Undecided = 4,
+}
+
+#[derive(Clone,PartialEq,Eq,Debug,Hash,FromProtobuf)]
+pub enum ActionResult {
+    Success = 1,
+    NotSupported = 2,
+    Error = 3,
+    CantQueueThatOrder = 4,
+    Retry = 5,
+    Cooldown = 6,
+    QueueIsFull = 7,
+    RallyQueueIsFull = 8,
+    NotEnoughMinerals = 9,
+    NotEnoughVespene = 10,
+    NotEnoughTerrazine = 11,
+    NotEnoughCustom = 12,
+    NotEnoughFood = 13,
+    FoodUsageImpossible = 14,
+    NotEnoughLife = 15,
+    NotEnoughShields = 16,
+    NotEnoughEnergy = 17,
+    LifeSuppressed = 18,
+    ShieldsSuppressed = 19,
+    EnergySuppressed = 20,
+    NotEnoughCharges = 21,
+    CantAddMoreCharges = 22,
+    TooMuchMinerals = 23,
+    TooMuchVespene = 24,
+    TooMuchTerrazine = 25,
+    TooMuchCustom = 26,
+    TooMuchFood = 27,
+    TooMuchLife = 28,
+    TooMuchShields = 29,
+    TooMuchEnergy = 30,
+    MustTargetUnitWithLife = 31,
+    MustTargetUnitWithShields = 32,
+    MustTargetUnitWithEnergy = 33,
+    CantTrade = 34,
+    CantSpend = 35,
+    CantTargetThatUnit = 36,
+    CouldntAllocateUnit = 37,
+    UnitCantMove = 38,
+    TransportIsHoldingPosition = 39,
+    BuildTechRequirementsNotMet = 40,
+    CantFindPlacementLocation = 41,
+    CantBuildOnThat = 42,
+    CantBuildTooCloseToDropOff = 43,
+    CantBuildLocationInvalid = 44,
+    CantSeeBuildLocation = 45,
+    CantBuildTooCloseToCreepSource = 46,
+    CantBuildTooCloseToResources = 47,
+    CantBuildTooFarFromWater = 48,
+    CantBuildTooFarFromCreepSource = 49,
+    CantBuildTooFarFromBuildPowerSource = 50,
+    CantBuildOnDenseTerrain = 51,
+    CantTrainTooFarFromTrainPowerSource = 52,
+    CantLandLocationInvalid = 53,
+    CantSeeLandLocation = 54,
+    CantLandTooCloseToCreepSource = 55,
+    CantLandTooCloseToResources = 56,
+    CantLandTooFarFromWater = 57,
+    CantLandTooFarFromCreepSource = 58,
+    CantLandTooFarFromBuildPowerSource = 59,
+    CantLandTooFarFromTrainPowerSource = 60,
+    CantLandOnDenseTerrain = 61,
+    AddOnTooFarFromBuilding = 62,
+    MustBuildRefineryFirst = 63,
+    BuildingIsUnderConstruction = 64,
+    CantFindDropOff = 65,
+    CantLoadOtherPlayersUnits = 66,
+    NotEnoughRoomToLoadUnit = 67,
+    CantUnloadUnitsThere = 68,
+    CantWarpInUnitsThere = 69,
+    CantLoadImmobileUnits = 70,
+    CantRechargeImmobileUnits = 71,
+    CantRechargeUnderConstructionUnits = 72,
+    CantLoadThatUnit = 73,
+    NoCargoToUnload = 74,
+    LoadAllNoTargetsFound = 75,
+    NotWhileOccupied = 76,
+    CantAttackWithoutAmmo = 77,
+    CantHoldAnyMoreAmmo = 78,
+    TechRequirementsNotMet = 79,
+    MustLockdownUnitFirst = 80,
+    MustTargetUnit = 81,
+    MustTargetInventory = 82,
+    MustTargetVisibleUnit = 83,
+    MustTargetVisibleLocation = 84,
+    MustTargetWalkableLocation = 85,
+    MustTargetPawnableUnit = 86,
+    YouCantControlThatUnit = 87,
+    YouCantIssueCommandsToThatUnit = 88,
+    MustTargetResources = 89,
+    RequiresHealTarget = 90,
+    RequiresRepairTarget = 91,
+    NoItemsToDrop = 92,
+    CantHoldAnyMoreItems = 93,
+    CantHoldThat = 94,
+    TargetHasNoInventory = 95,
+    CantDropThisItem = 96,
+    CantMoveThisItem = 97,
+    CantPawnThisUnit = 98,
+    MustTargetCaster = 99,
+    CantTargetCaster = 100,
+    MustTargetOuter = 101,
+    CantTargetOuter = 102,
+    MustTargetYourOwnUnits = 103,
+    CantTargetYourOwnUnits = 104,
+    MustTargetFriendlyUnits = 105,
+    CantTargetFriendlyUnits = 106,
+    MustTargetNeutralUnits = 107,
+    CantTargetNeutralUnits = 108,
+    MustTargetEnemyUnits = 109,
+    CantTargetEnemyUnits = 110,
+    MustTargetAirUnits = 111,
+    CantTargetAirUnits = 112,
+    MustTargetGroundUnits = 113,
+    CantTargetGroundUnits = 114,
+    MustTargetStructures = 115,
+    CantTargetStructures = 116,
+    MustTargetLightUnits = 117,
+    CantTargetLightUnits = 118,
+    MustTargetArmoredUnits = 119,
+    CantTargetArmoredUnits = 120,
+    MustTargetBiologicalUnits = 121,
+    CantTargetBiologicalUnits = 122,
+    MustTargetHeroicUnits = 123,
+    CantTargetHeroicUnits = 124,
+    MustTargetRoboticUnits = 125,
+    CantTargetRoboticUnits = 126,
+    MustTargetMechanicalUnits = 127,
+    CantTargetMechanicalUnits = 128,
+    MustTargetPsionicUnits = 129,
+    CantTargetPsionicUnits = 130,
+    MustTargetMassiveUnits = 131,
+    CantTargetMassiveUnits = 132,
+    MustTargetMissile = 133,
+    CantTargetMissile = 134,
+    MustTargetWorkerUnits = 135,
+    CantTargetWorkerUnits = 136,
+    MustTargetEnergyCapableUnits = 137,
+    CantTargetEnergyCapableUnits = 138,
+    MustTargetShieldCapableUnits = 139,
+    CantTargetShieldCapableUnits = 140,
+    MustTargetFlyers = 141,
+    CantTargetFlyers = 142,
+    MustTargetBuriedUnits = 143,
+    CantTargetBuriedUnits = 144,
+    MustTargetCloakedUnits = 145,
+    CantTargetCloakedUnits = 146,
+    MustTargetUnitsInAStasisField = 147,
+    CantTargetUnitsInAStasisField = 148,
+    MustTargetUnderConstructionUnits = 149,
+    CantTargetUnderConstructionUnits = 150,
+    MustTargetDeadUnits = 151,
+    CantTargetDeadUnits = 152,
+    MustTargetRevivableUnits = 153,
+    CantTargetRevivableUnits = 154,
+    MustTargetHiddenUnits = 155,
+    CantTargetHiddenUnits = 156,
+    CantRechargeOtherPlayersUnits = 157,
+    MustTargetHallucinations = 158,
+    CantTargetHallucinations = 159,
+    MustTargetInvulnerableUnits = 160,
+    CantTargetInvulnerableUnits = 161,
+    MustTargetDetectedUnits = 162,
+    CantTargetDetectedUnits = 163,
+    CantTargetUnitWithEnergy = 164,
+    CantTargetUnitWithShields = 165,
+    MustTargetUncommandableUnits = 166,
+    CantTargetUncommandableUnits = 167,
+    MustTargetPreventDefeatUnits = 168,
+    CantTargetPreventDefeatUnits = 169,
+    MustTargetPreventRevealUnits = 170,
+    CantTargetPreventRevealUnits = 171,
+    MustTargetPassiveUnits = 172,
+    CantTargetPassiveUnits = 173,
+    MustTargetStunnedUnits = 174,
+    CantTargetStunnedUnits = 175,
+    MustTargetSummonedUnits = 176,
+    CantTargetSummonedUnits = 177,
+    MustTargetUser1 = 178,
+    CantTargetUser1 = 179,
+    MustTargetUnstoppableUnits = 180,
+    CantTargetUnstoppableUnits = 181,
+    MustTargetResistantUnits = 182,
+    CantTargetResistantUnits = 183,
+    MustTargetDazedUnits = 184,
+    CantTargetDazedUnits = 185,
+    CantLockdown = 186,
+    CantMindControl = 187,
+    MustTargetDestructibles = 188,
+    CantTargetDestructibles = 189,
+    MustTargetItems = 190,
+    CantTargetItems = 191,
+    NoCalldownAvailable = 192,
+    WaypointListFull = 193,
+    MustTargetRace = 194,
+    CantTargetRace = 195,
+    MustTargetSimilarUnits = 196,
+    CantTargetSimilarUnits = 197,
+    CantFindEnoughTargets = 198,
+    AlreadySpawningLarva = 199,
+    CantTargetExhaustedResources = 200,
+    CantUseMinimap = 201,
+    CantUseInfoPanel = 202,
+    OrderQueueIsFull = 203,
+    CantHarvestThatResource = 204,
+    HarvestersNotRequired = 205,
+    AlreadyTargeted = 206,
+    CantAttackWeaponsDisabled = 207,
+    CouldntReachTarget = 208,
+    TargetIsOutOfRange = 209,
+    TargetIsTooClose = 210,
+    TargetIsOutOfArc = 211,
+    CantFindTeleportLocation = 212,
+    InvalidItemClass = 213,
+    CantFindCancelOrder = 214,
+}
+
 #[derive(Debug, FromProtobuf)]
 pub struct ActionError {
     /// Only populated when using raw interface
@@ -553,7 +879,7 @@ pub struct ActionError {
     #[Get]
     pub ability_id: u64,
     #[Get]
-    pub result: protos::ActionResult,
+    pub result: ActionResult,
 }
 
 #[derive(Debug, FromProtobuf)]
@@ -639,12 +965,34 @@ pub struct PowerSource {
     pub tag: u64,
 }
 
+#[derive(Clone,PartialEq,Eq,Debug,Hash,FromProtobuf)]
+pub enum DisplayType {
+    Visible = 1,
+    Snapshot = 2,
+    Hidden = 3,
+}
+
+#[derive(Clone,PartialEq,Eq,Debug,Hash,FromProtobuf)]
+pub enum Alliance {
+    Selff = 1,
+    Ally = 2,
+    Neutral = 3,
+    Enemy = 4,
+}
+
+#[derive(Clone,PartialEq,Eq,Debug,Hash,FromProtobuf)]
+pub enum CloakState {
+    Cloaked = 1,
+    CloakedDetected = 2,
+    NotCloaked = 3,
+}
+
 #[derive(Debug, FromProtobuf)]
 pub struct Unit {
     #[Get]
-    pub display_type: protos::DisplayType,
+    pub display_type: DisplayType,
     #[Get]
-    pub alliance: protos::Alliance,
+    pub alliance: Alliance,
     /// Unique identifier for a unix
     #[Get]
     pub tag: u64,
@@ -662,7 +1010,7 @@ pub struct Unit {
     #[Get]
     pub build_progress: f32,
     #[Get]
-    pub cloak: protos::CloakState,
+    pub cloak: CloakState,
 
     #[Get]
     pub detect_range: f32,
@@ -761,12 +1109,18 @@ pub struct UnitOrder {
     pub progress: f32,
 }
 
+#[derive(Clone,PartialEq,Eq,Debug,Hash,FromProtobuf)]
+pub enum Alert {
+    NuclearLaunchDetected = 1,
+    NydusWormDetected = 2,
+}
+
 #[derive(Debug, FromProtobuf)]
 pub struct Observation {
     #[Get]
     pub game_loop: u32,
     pub player_common: PlayerCommon,
-    pub alerts: Vec<protos::Alert>,
+    pub alerts: Vec<Alert>,
     /// Abilities available in the selection. Enabled if in this list, disabled otherwise
     pub abilities: Vec<AvailableAbility>,
     //TODO score: Score,
@@ -781,10 +1135,9 @@ pub struct PlayerResult {
     #[Get]
     pub player_id: u32,
     #[Get]
-    pub result: protos::Result,
+    pub result: ProtoResult,
 }
 
-use self::protos::Result as ProtoResult;
 
 
 #[derive(Debug, FromProtobuf)]
@@ -805,41 +1158,28 @@ pub struct ResponseObservation {
 
 impl ObservationRaw {
     pub fn get_my_units<'a>(&'a self) -> Vec<&'a Unit> {
-        self.units.iter().filter(|u| u.alliance == protos::Alliance::Selff).collect()
+        self.units.iter().filter(|u| u.alliance == Alliance::Selff).collect()
     }
 }
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, FromProtobuf)]
 pub enum Status {
     /// Game has been launched and is not yet doing anything
-    Launched = 1,
+    #[name="launched"] Launched = 1,
     /// Create game has been called, and the host is awaiting players
-    InitGame = 2,
+    #[name="init_game"] InitGame = 2,
     /// In a single or multiplayer game
-    InGame = 3,
+    #[name="in_game"] InGame = 3,
     /// In a replay
-    InReplay = 4,
+    #[name="in_replay"] InReplay = 4,
     /// Game has ended, can still request game info, but ready for a new game
-    Ended = 5,
+    #[name="ended"] Ended = 5,
     /// Application is shutting down
-    Quit = 6,
+    #[name="quit"] Quit = 6,
     /// Should not happen
-    Unknown = 99,
+    #[name="unknown"] Unknown = 99,
 }
 
-impl FromProtobuf<protos::Status> for Status {
-    fn from_protobuf(t: protos::Status) -> Result<Self, failure::Error> {
-        Ok(match t {
-            protos::Status::launched => Status::Launched,
-            protos::Status::init_game => Status::InitGame,
-            protos::Status::in_game => Status::InGame,
-            protos::Status::in_replay => Status::InReplay,
-            protos::Status::ended => Status::Ended,
-            protos::Status::quit => Status::Quit,
-            protos::Status::unknown => Status::Unknown,
-        })
-    }
-}
 
 
 #[derive(Debug, FromProtobuf)]
