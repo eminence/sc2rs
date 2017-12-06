a749536febb97636638592bf7602c01143948883
Andrew Chin <achin@eminence32.net>
2017-12-05 19:50:32 -0500

    Better build script for protobuf files

---
+++ build.rs
@@ -0,0 +1,43 @@
+extern crate protoc_rust;
+
+use std::fs::{File, read_dir};
+use std::io::{Read, Write};
+
+fn main() {
+    println!("cargo:rerun-if-changed=s2client-proto/s2clientprotocol/");
+    for entry in read_dir("s2client-proto/s2clientprotocol").unwrap() {
+        let entry = entry.unwrap();
+        println!("cargo:rerun-if-changed={}", entry.path().display());
+    }
+    println!("Generating protobuf files...");
+    protoc_rust::run(protoc_rust::Args {
+        out_dir: "sc2-protobuf/src/protos",
+        input: &["s2client-proto/s2clientprotocol/common.proto",
+            "s2client-proto/s2clientprotocol/data.proto",
+            "s2client-proto/s2clientprotocol/debug.proto",
+            "s2client-proto/s2clientprotocol/error.proto",
+            "s2client-proto/s2clientprotocol/query.proto",
+            "s2client-proto/s2clientprotocol/raw.proto",
+            "s2client-proto/s2clientprotocol/sc2api.proto",
+            "s2client-proto/s2clientprotocol/spatial.proto",
+            "s2client-proto/s2clientprotocol/score.proto",
+            "s2client-proto/s2clientprotocol/ui.proto"],
+        includes: &["s2client-proto"],
+    }).expect("protoc");
+
+    // patch up raw.rs, since the protobuf files include a field named "Self"
+    // which is not valid rust
+
+    let s = {
+        let mut raw = File::open("sc2-protobuf/src/protos/raw.rs").unwrap();
+        let mut s = String::new();
+        raw.read_to_string(&mut s).unwrap();
+        s
+    };
+
+    let new_s = s.replace("Alliance::Self", "Alliance::Selff");
+    let new_s = new_s.replace("Self = 1,", "Selff = 1,");
+    let mut raw = File::create("sc2-protobuf/src/protos/raw.rs").unwrap();
+    raw.write_all(new_s.as_bytes()).unwrap();
+
+}
